9879ac32ece53d6a9a51abe8833fc525d510ebf6
Kamran <136312141+BayramovKamran@users.noreply.github.com>
2024-11-01 13:29:11 +0300

    Update CourseDao.java

--- src/main/java/org/example/dao/CourseDao.java
+++ src/main/java/org/example/dao/CourseDao.java
@@ -5,4 +5,14 @@ import org.example.model.Course;
 import java.util.List;
 
 public interface CourseDao {
+  
+    void createCourse(Course course);
+
+    Course getCourseById(int id);
+
+    List<Course> getAllCourses();
+
+    void updateCourse(Course course);
+
+    void deleteCourse(int id);
 }
